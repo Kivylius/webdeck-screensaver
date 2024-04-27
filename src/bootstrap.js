351108a96c4a230baddaae59a39be1ff82e72745
Cezarijus Kivylius <ltcezaris@gmail.com>
2024-04-27 10:11:57 +0100

    Initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,11 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Nothing needs to chagne here.
+// Make all changes in <App> component.
+
+ReactDOM.render(
+  <App config={{}} setConfig={() => {}} />,
+  document.getElementById("root")
+);
